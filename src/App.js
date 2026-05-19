dd457f72ab43301e9466db3624bacb6c983cb8dc
Louis Obiomon <louisbioms20@gmail.com>
2026-05-19 14:24:11 +0100

    App.js - router with all pages wired up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider }    from "./context/AuthContext";
+import { SocketProvider }  from "./context/SocketContext";
+import { ThemeProvider }   from "./context/ThemeContext";
+import ProtectedRoute      from "./components/layout/ProtectedRoute";
+import LoginPage           from "./pages/LoginPage";
+import SignupPage          from "./pages/SignupPage";
+import InboxPage           from "./pages/InboxPage";
+import ProfilePage         from "./pages/ProfilePage";
+import AdminDashboard      from "./pages/AdminDashboard";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <AuthProvider>
+        <SocketProvider>
+          <BrowserRouter>
+            <Routes>
+              {/* Public */}
+              <Route path="/login"  element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+
+              {/* Protected */}
+              <Route path="/inbox" element={
+                <ProtectedRoute><InboxPage /></ProtectedRoute>
+              } />
+              <Route path="/inbox/:id" element={
+                <ProtectedRoute><InboxPage /></ProtectedRoute>
+              } />
+              <Route path="/profile" element={
+                <ProtectedRoute><ProfilePage /></ProtectedRoute>
+              } />
+              <Route path="/admin" element={
+                <ProtectedRoute roles={["admin"]}><AdminDashboard /></ProtectedRoute>
+              } />
+
+              {/* Fallback */}
+              <Route path="*" element={<Navigate to="/inbox" replace />} />
+            </Routes>
+          </BrowserRouter>
+        </SocketProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
-
-export default App;
